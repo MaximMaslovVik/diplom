807a1fc5e841089a70b07b64db04bc30cd06a6da
Максим Маслов <flyover31@yandex.ru>
2020-03-18 23:52:58 +0300

    роутеры переписывал

--- app.js
+++ app.js
@@ -5,15 +5,11 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 
 const app = express();
+const index = require('./routes/index');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT } = require('./secret');
-
-const users = require('./routes/users');
-const articles = require('./routes/articles');
-const auth = require('./middlewares/auth');
-
 
 mongoose.connect('mongodb://localhost:27017/news-apidb', {
   useNewUrlParser: true,
@@ -28,24 +24,9 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Что то пошло не так, загрузка сервера  прервана');
-  }, 0);
-});
+app.use('/', index);
 
-app.use(auth);
-app.use('/', users);
-app.use('/', articles);
-
-app.all('/*', (req, res) => res.status(404).send('Запрашиваемый ресурс не найден'));
-
-app.use(errorLogger);
 app.use(errors());
-
-app.use((err, req, res, next) => {
-  res.status(err.statusCode || 500).send({ message: err.message || 'На сервере произошла ошибка' });
-  next();
-});
+app.use(errorLogger);
 
 app.listen(PORT, () => {});
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+const auth = require('../middlewares/auth');
+
+const signin = require('./signin');
+const signup = require('./signup');
+const users = require('./users');
+const articles = require('./articles');
+const crashTest = require('./crash-error');
+
+
+app.use('/', signin);
+app.use('/', signup);
+app.use('/', auth, users);
+app.use('/', auth, articles);
+
+app.use('/', crashTest);
+
+
+app.all('/*', (req, res) => res.status(404).send('Запрашиваемый ресурс не найден'));
+
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).send({ message: err.message || 'На сервере произошла ошибка' });
+  next();
+});
+
+module.exports = app;
