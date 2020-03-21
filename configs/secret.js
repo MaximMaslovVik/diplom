761681de313e75d6d40d2b358c4f20ef0e656013
Максим Маслов <flyover31@yandex.ru>
2020-03-22 00:36:27 +0300

    исправлял секретность ключа

--- configs/secret.js
+++ configs/secret.js
@@ -1,25 +1,23 @@
 require('dotenv').config();
 
 const {
-  NODE_ENV, JWT_SECRET, DB_HOST, PORT, SECRET,
+  NODE_ENV, DB_HOST, SECRET,
 } = process.env;
 const DEV_SECRET = 'secret key';
-const DEV_DB_HOST = 'mongodb://localhost:27017/my-database';
-const DEV_PORT = 3000;
+const DEV_DB_HOST = 'mongodb://localhost:27017/news-apidb';
 
 const DB = NODE_ENV === 'production' && DB_HOST ? DB_HOST : DEV_DB_HOST;
-const SERVER_PORT = NODE_ENV === 'production' && PORT ? PORT : DEV_PORT;
 const SECRET_STRING = NODE_ENV === 'production' && SECRET ? SECRET : DEV_SECRET;
+/*
+}); */
 
 console.log({
   DB,
-  SERVER_PORT,
 });
 
 module.exports.JWT_SECRET = process.env.JWT_SECRET || 'secret key';
-
+module.exports.PORT = process.env.PORT || 3000;
 module.exports = {
   DB,
-  SERVER_PORT,
   SECRET_STRING,
 };
