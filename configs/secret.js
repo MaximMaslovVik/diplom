bba58dedb059649beeb82c5dcfd47ec3b4ec57c6
Максим Маслов <flyover31@yandex.ru>
2020-03-22 00:41:31 +0300

    Add files via upload

--- configs/secret.js
+++ configs/secret.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
 
 const {
-  NODE_ENV, JWT_SECRET, DB_HOST, PORT, SECRET,
+  NODE_ENV, DB_HOST, SECRET,
 } = process.env;
 const DEV_SECRET = 'secret key';
 const DEV_DB_HOST = 'mongodb://localhost:27017/news-apidb';
 
 const DB = NODE_ENV === 'production' && DB_HOST ? DB_HOST : DEV_DB_HOST;
-
 const SECRET_STRING = NODE_ENV === 'production' && SECRET ? SECRET : DEV_SECRET;
+/*
+}); */
+
+console.log({
+  DB,
+});
 
 module.exports.JWT_SECRET = process.env.JWT_SECRET || 'secret key';
 module.exports.PORT = process.env.PORT || 3000;
