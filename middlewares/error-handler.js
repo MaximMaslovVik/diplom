b3c473b0762d97a75737a445d0b2dd4adf74cf87
Максим Маслов <flyover31@yandex.ru>
2020-03-22 21:40:31 +0300

    Add files via upload

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,7 +1,8 @@
 const { SERVER_ERROR } = require('../configs/constants');
 
-const errorHandler = (error, req, res) => {
-  const { statusCode = 500, message } = error;
+const errorHandler = (err, req, res, next) => {
+  console.log('the error comes from error handler');
+  const { statusCode = 500, message } = err;
   res.status(statusCode).send({ message: statusCode === 500 ? SERVER_ERROR : message });
 };
 
