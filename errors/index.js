e44202e7003dded6ac0107a7edc7799d9f902760
Максим Маслов <flyover31@yandex.ru>
2020-03-28 20:30:56 +0300

    Add files via upload

---
+++ errors/index.js
@@ -0,0 +1,13 @@
+const ErrorReq = require('./error-req');
+const ErrorForbidden = require('./error-forbidden');
+const ErrorNotFound = require('./error-notFound');
+const Error409 = require('./error-409');
+const ErrorAuth = require('./error-auth');
+
+module.exports = {
+  ErrorReq,
+  ErrorForbidden,
+  ErrorNotFound,
+  Error409,
+  ErrorAuth,
+};
