daed3bdebdaeac13f091982baed558cf121b9245
Максим Маслов <flyover31@yandex.ru>
2020-03-31 11:44:54 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,17 +5,29 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/error-handler');
 
+const mongoose = require('mongoose');
+
 const app = express();
 const index = require('./routes/routes');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-app.use(requestLogger);
+const {
+  MONGODB = 'mongodb://localhost:27017/news_api',
+} = process.env;
+
+mongoose.connect(MONGODB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(requestLogger);
 
 app.use('/', index);
 
