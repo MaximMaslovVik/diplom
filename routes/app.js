dd68ab135f5bf0f0264045346b56edabc50afce1
Максим Маслов <flyover31@yandex.ru>
2020-04-15 16:19:22 +0300

    Delete app.js

--- routes/app.js
+++
@@ -1,7 +0,0 @@
-const errorRouter = require('express').Router();
-const { ErrorNotFound } = require('../errors/index');
-const { NOT_FOUND } = require('../configs/constants');
-
-errorRouter.all(new ErrorNotFound(NOT_FOUND));
-
-module.exports = errorRouter;
