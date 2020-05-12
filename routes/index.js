d6a92a11f5a147b0e2346ec5355807241df5f630
Максим Маслов <flyover31@yandex.ru>
2020-05-13 01:11:38 +0300

    чинил cors

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,10 @@ const users = require('./users');
 const articles = require('./articles');
 const errorApp = require('./errorall');
 
-const crashTest = require('./crash-text');
-
 app.use('/signin', signin);
 app.use('/signup', signup);
 app.use('/users/me', auth, users);
 app.use('/', auth, articles);
 app.use('/*', errorApp);
-app.use('/', crashTest);
 
 module.exports = app;
